1ebd3a76eb366ed7378b45eec380e23bfecfc370
Peter Levi <peterlevi@peterlevi.com>
2018-08-27 01:25:42 +0300

    Fix protocol for https

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import json
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
 @app.route("/")
 def index():
-    return render_template('index.html',)
+    return render_template('index.html', )
+
 
 @socketio.on('send_message')
 def handle_source(json_data):
     text = json_data['message'].encode('ascii', 'ignore')
     socketio.emit('echo', {'echo': 'Server Says: ' + text.decode()})
+
 
 if __name__ == "__main__":
     socketio.run(app)
